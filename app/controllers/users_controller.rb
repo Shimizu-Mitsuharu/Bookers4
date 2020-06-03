e6da3138096268716c63ce95c95f1690d31d61ed
vagrant <vagrant@localhost.localdomain>
2020-06-03 07:50:00 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,12 +25,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.follows.all
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  #url直接防止　メソッドを自己定義してbefore_actionで発動。
    def baria_user
     unless params[:id].to_i == current_user.id
       redirect_to user_path(current_user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,17 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :follows, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   attachment :profile_image, destroy: false
 
-  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  def followed_by?(user)
+    passive_relationships.where(following_id: user.id).exists?
+  end
 end
